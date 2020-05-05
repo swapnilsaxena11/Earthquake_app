81a625debc5badb9dd4b7d656a553fde4fcf1d93
tintin-quarantino <31161899+tintin-quarantino@users.noreply.github.com>
2020-05-05 12:25:43 +0530

    Update EarthquakeActivity.java
    added asynctask class for parallel execution of  httprequest and userinput

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -1,22 +1,8 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -24,48 +10,57 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private infoAdapter adapter;
+    private String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
-
-        // Create a fake list of earthquake locations.
-        ArrayList<location_info> earthquakes = QueryUtils.extractEarthquakes();
-        //earthquakes.add(new location_info("9.1","San Francisco"," April 30,2020"));
-        //earthquakes.add(new location_info("5.1","New Delhi"," May 3,2020"));
-        //earthquakes.add(new location_info("7.3","Oklahama"," April 6,2020"));
-
-        // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
-
-        // Create a new {@link ArrayAdapter} of earthquakes
-        final infoAdapter adapter = new infoAdapter(this, earthquakes);
-
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
+        adapter = new infoAdapter(this, new ArrayList<location_info>());
         earthquakeListView.setAdapter(adapter);
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // Find the current earthquake that was clicked on
                 location_info currentEarthquake = adapter.getItem(position);
-
-                // Convert the String URL into a URI object (to pass into the Intent constructor)
                 Uri earthquakeUri = Uri.parse(currentEarthquake.getmUrl());
-
-                // Create a new intent to view the earthquake URI
                 Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
-
-                // Send the intent to launch a new activity
                 startActivity(websiteIntent);
             }
         });
+        TaskAsync task = new TaskAsync();
+        task.execute(USGS_REQUEST_URL);
+    }
+    private class TaskAsync extends AsyncTask<String,Void,ArrayList<location_info>>{
+
+        @Override
+        protected ArrayList<location_info> doInBackground(String... url) {
+            try {
+                return QueryUtils.fetchEarthquakeData(url[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<location_info> location_info) {
+            // Clear the adapter of previous earthquake data
+            adapter.clear();
+
+            
+            if (location_info != null && !location_info.isEmpty()) {
+                adapter.addAll(location_info);
+            }
+        }
     }
 }
